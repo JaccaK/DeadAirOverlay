55dca468bd8e8f2a77ed19252606d734aeb94ecd
JaccaK <79811354+JaccaK@users.noreply.github.com>
2024-11-23 04:13:45 -0800

    Manually uploading the repo because I'm apparently not great at git
    Protip!

---
+++ forge.config.js
@@ -0,0 +1,54 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+
+module.exports = {
+  packagerConfig: {
+    name: 'Dead Air Overlay',
+    asar: true,
+    osxSign: {},
+    appCategoryType: 'public.app-category.developer-tools',
+    icon: 'icon.png'
+  },
+  rebuildConfig: {
+  force: true
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {
+        options: {
+          icon: 'icon.png'
+        }
+      },
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-auto-unpack-natives',
+      config: {},
+    },
+    // Fuses are used to enable/disable various Electron functionality
+    // at package time, before code signing the application
+    new FusesPlugin({
+      version: FuseVersion.V1,
+      [FuseV1Options.RunAsNode]: true,
+      [FuseV1Options.EnableCookieEncryption]: true,
+      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+      [FuseV1Options.EnableNodeCliInspectArguments]: true,
+      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: false,
+      [FuseV1Options.OnlyLoadAppFromAsar]: false,
+    }),
+  ],
+};
